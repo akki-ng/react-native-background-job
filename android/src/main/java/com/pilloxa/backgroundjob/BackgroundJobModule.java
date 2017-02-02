9fcb792289ec27c9785e4502dad8313aa4809794
akki-ng <akshay.sharma@neogrowth.in>
2017-02-02 15:50:02 +0530

    One Time Job / Repeated Job added

---
+++ android/src/main/java/com/pilloxa/backgroundjob/BackgroundJobModule.java
@@ -0,0 +1,275 @@
+
+package com.pilloxa.backgroundjob;
+
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.ComponentName;
+import android.content.Context;
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.util.Log;
+import com.facebook.react.bridge.*;
+import com.facebook.react.bridge.Arguments;
+
+import javax.annotation.Nullable;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class BackgroundJobModule extends ReactContextBaseJavaModule implements LifecycleEventListener {
+
+    private String LOG_TAG = "backgroundjob";
+    private static final String NETWORK_TYPE_UNMETERED = "UNMETERED";
+    private static final String NETWORK_TYPE_NONE = "NONE";
+    private static final String NETWORK_TYPE_ANY = "ANY";
+
+    private final ReactApplicationContext reactContext;
+
+    private List<JobInfo> mJobs;
+
+    private JobScheduler jobScheduler;
+
+    private boolean mInitialized = false;
+
+    @Override
+    public void initialize() {
+        Log.d(LOG_TAG, "Initializing BackgroundJob");
+        if (jobScheduler == null) {
+            jobScheduler = (JobScheduler) getReactApplicationContext().getSystemService(Context.JOB_SCHEDULER_SERVICE);
+            mJobs = jobScheduler.getAllPendingJobs();
+            mInitialized = true;
+        }
+        super.initialize();
+        getReactApplicationContext().addLifecycleEventListener(this);
+    }
+
+    public BackgroundJobModule(ReactApplicationContext reactContext) {
+        super(reactContext);
+        this.reactContext = reactContext;
+    }
+
+    private JobInfo createJobObject(int taskId, String jobKey,
+                         int timeout,
+                         int period,
+                         boolean persist,
+                         int networkType,
+                         boolean requiresCharging,
+                         boolean requiresDeviceIdle,
+                         String payLoad) {
+        int persistInt = persist ? 1 : 0;
+
+        ComponentName componentName = new ComponentName(getReactApplicationContext(), BackgroundJob.class.getName());
+        PersistableBundle jobExtras = new PersistableBundle();
+        JobInfo.Builder jobInfo = new JobInfo.Builder(taskId, componentName);
+
+        if(period > 0) {
+            jobExtras.putInt("period", period);
+            jobInfo.setPeriodic(period);
+        }
+
+        jobExtras.putString("jobKey", jobKey);
+        jobExtras.putInt("timeout", timeout);
+        jobExtras.putInt("persist", persistInt);
+        jobExtras.putInt("networkType", networkType);
+        jobExtras.putInt("requiresCharging", requiresCharging ? 1 : 0);
+        jobExtras.putInt("requiresDeviceIdle", requiresDeviceIdle ? 1 : 0);
+        jobExtras.putString("payLoad", payLoad);
+
+
+        jobInfo.setExtras(jobExtras);
+        jobInfo.setRequiresDeviceIdle(requiresDeviceIdle);
+        jobInfo.setRequiresCharging(requiresCharging);
+        jobInfo.setPersisted(persist);
+
+        jobInfo.setRequiredNetworkType(networkType);
+
+        return jobInfo.build();
+    }
+
+    private int scheduleJob(int taskId, String jobKey,
+                         int timeout,
+                         int period,
+                         boolean persist,
+                         boolean appActive,
+                         int networkType,
+                         boolean requiresCharging,
+                         boolean requiresDeviceIdle,
+                         String payLoad) {
+        JobInfo newJob = this.createJobObject(taskId, jobKey, timeout, period, persist, networkType, requiresCharging, requiresDeviceIdle, payLoad);
+
+        for (JobInfo iJobInfo : mJobs) {
+            if (iJobInfo.getId() == taskId) {
+                mJobs.remove(iJobInfo);
+            }
+        }
+        mJobs.add(newJob);
+
+        if (!appActive) {
+            scheduleJobs();
+        }
+        return newJob.getId();
+    }
+
+    @ReactMethod
+    public int scheduleRepeatedJob(int taskId, String jobKey,
+                         int timeout,
+                         int period,
+                         boolean persist,
+                         boolean appActive,
+                         int networkType,
+                         boolean requiresCharging,
+                         boolean requiresDeviceIdle,
+                         String payLoad) {
+
+        Log.v(LOG_TAG, String.format("Scheduling Repeated Job: %s, JobId: %s, timeout: %s, period: %s, network type: %s, requiresCharging: %s, requiresDeviceIdle: %s", jobKey, taskId, timeout, period, networkType, requiresCharging, requiresDeviceIdle));
+
+       return scheduleJob(taskId, jobKey, timeout, period, persist, appActive, networkType, requiresCharging, requiresDeviceIdle, payLoad);
+    }
+
+    @ReactMethod
+    public int scheduleOneTimeJob(int taskId, String jobKey,
+                         int timeout,
+                         boolean persist,
+                         boolean appActive,
+                         int networkType,
+                         boolean requiresCharging,
+                         boolean requiresDeviceIdle,
+                         String payLoad) {
+        Log.v(LOG_TAG, String.format("Scheduling One Time Job: %s, JobId: %s, timeout: %s, network type: %s, requiresCharging: %s, requiresDeviceIdle: %s", jobKey, taskId, timeout, networkType, requiresCharging, requiresDeviceIdle));
+
+        return scheduleJob(taskId, jobKey, timeout, -1, persist, appActive, networkType, requiresCharging, requiresDeviceIdle, payLoad);
+    }
+
+    @ReactMethod
+    private boolean hardCancel(int taskId ) {
+        mJobs = jobScheduler.getAllPendingJobs();
+        //-1(TaskId not Found) , 0(TaskId found, jobKey doesnot match), 1(TaskId found, jobKey matched)
+        for (JobInfo iJobInfo : mJobs) {
+            if (iJobInfo.getId() == taskId) {
+                String storedJobKey = iJobInfo.getExtras().containsKey("jobKey") ? iJobInfo.getExtras().getString("jobKey") : null;
+
+                Log.d(LOG_TAG, "Hard Cancelling job: " + String.valueOf(storedJobKey) + " (" + taskId + ")");
+                jobScheduler.cancel(taskId);
+                mJobs = jobScheduler.getAllPendingJobs();
+                return true;
+            }
+        }
+        Log.d(LOG_TAG, "Cancelling job:Failure: " + taskId + " does not exist");
+        return false;
+    }
+
+    @ReactMethod
+    private boolean cancel(String jobKey, int taskId ) {
+        mJobs = jobScheduler.getAllPendingJobs();
+        //-1(TaskId not Found) , 0(TaskId found, jobKey doesnot match), 1(TaskId found, jobKey matched)
+        int isValid = -1;
+        JobInfo iJobInfo = null;
+        for (JobInfo jobInfo : mJobs) {
+            if (jobInfo.getId() == taskId) {
+                PersistableBundle extras = jobInfo.getExtras();
+                if(extras.containsKey("jobKey") && jobKey.equals(extras.getString("jobKey")) ) {
+                    isValid = 1;
+                }else {
+                    isValid = 0;
+                }
+                iJobInfo = jobInfo;
+                break;
+            }
+        }
+
+        if(isValid == 1) {
+            Log.d(LOG_TAG, "Cancelling job: " + jobKey + " (" + taskId + ")");
+            jobScheduler.cancel(taskId);
+            mJobs = jobScheduler.getAllPendingJobs();
+            return true;
+        }else if(isValid == 0){
+            String storedJobKey = iJobInfo != null && iJobInfo.getExtras().containsKey("jobKey") ? iJobInfo.getExtras().getString("jobKey") : null;
+            Log.d(LOG_TAG, "Cancelling job:Failure: " + taskId + " existes but JobKey(" + String.valueOf(storedJobKey) + ") doesnot match to provided jobkey(" + jobKey + ")");
+        }else {
+            Log.d(LOG_TAG, "Cancelling job:Failure: " + taskId + " does not exist");
+        }
+        return false;
+    }
+
+    @ReactMethod
+    public void cancelAll() {
+        Log.d(LOG_TAG, "Cancelling all jobs");
+        jobScheduler.cancelAll();
+        mJobs = jobScheduler.getAllPendingJobs();
+    }
+
+    private WritableArray _getAll() {
+        Log.d(LOG_TAG, "Getting all jobs");
+        WritableArray jobs = Arguments.createArray();
+        if (mJobs != null) {
+            for (JobInfo job : mJobs) {
+                Log.d(LOG_TAG, "Fetching job " + job.getId());
+                Bundle extras = new Bundle(job.getExtras());
+                WritableMap jobMap = Arguments.fromBundle(extras);
+                jobMap.putBoolean("persist", extras.getInt("persist") == 1);
+                jobMap.putBoolean("requiresCharging", extras.getInt("requiresCharging") == 1);
+                jobMap.putBoolean("requiresDeviceIdle", extras.getInt("requiresDeviceIdle") == 1);
+                jobs.pushMap(jobMap);
+            }
+        }
+
+        return jobs;
+    }
+
+    @ReactMethod
+    public void getAll(Callback callback) {
+        WritableArray jobs = _getAll();
+        callback.invoke(jobs);
+    }
+
+    @Override
+    public String getName() {
+        return "BackgroundJob";
+    }
+
+    @Nullable
+    @Override
+    public Map<String, Object> getConstants() {
+        Log.d(LOG_TAG, "Getting constants");
+        jobScheduler = (JobScheduler) getReactApplicationContext().getSystemService(Context.JOB_SCHEDULER_SERVICE);
+        if (jobScheduler != null) {
+            mJobs = jobScheduler.getAllPendingJobs();
+            mInitialized = true;
+        }
+        HashMap<String, Object> constants = new HashMap<>();
+        constants.put("jobs", _getAll());
+        constants.put(NETWORK_TYPE_UNMETERED, JobInfo.NETWORK_TYPE_UNMETERED);
+        constants.put(NETWORK_TYPE_ANY, JobInfo.NETWORK_TYPE_ANY);
+        constants.put(NETWORK_TYPE_NONE, JobInfo.NETWORK_TYPE_NONE);
+        return constants;
+    }
+
+    @Override
+    public void onHostResume() {
+        Log.d(LOG_TAG, "Woke up");
+        mJobs = jobScheduler.getAllPendingJobs();
+        jobScheduler.cancelAll();
+
+    }
+
+    private void scheduleJobs() {
+        for (JobInfo job : mJobs) {
+            Log.d(LOG_TAG, "Sceduling job " + job.getId());
+            jobScheduler.cancel(job.getId());
+            int result = jobScheduler.schedule(job);
+            if (result == JobScheduler.RESULT_SUCCESS)
+                Log.d(LOG_TAG, "Job (" + job.getId() + ") scheduled successfully!");
+        }
+    }
+
+    @Override
+    public void onHostPause() {
+        Log.d(LOG_TAG, "Pausing");
+        scheduleJobs();
+    }
+
+    @Override
+    public void onHostDestroy() {
+        Log.d(LOG_TAG, "Destroyed");
+    }
+}
---
+++ index.js
@@ -0,0 +1,291 @@
+"use strict";
+import { NativeModules, AppRegistry } from "react-native";
+
+const AppState = NativeModules.AppState;
+const tag = "BackgroundJob:";
+const jobModule = NativeModules.BackgroundJob;
+const nativeJobs = jobModule.jobs;
+var registeredJobs = {};
+var alreadyScheduled = {};
+var globalWarning = __DEV__;
+
+const BackgroundJob = {
+  NETWORK_TYPE_UNMETERED: jobModule.UNMETERED,
+  NETWORK_TYPE_NONE: jobModule.NONE,
+  NETWORK_TYPE_ANY: jobModule.ANY,
+  /**
+     * Registers jobs and the functions they should run.
+     *
+     * This has to run on each initialization of React Native. Only doing this will not start running the job. It has to be scheduled by `schedule` to start running.
+     *
+     * @param {Object} obj
+     * @param {string} obj.jobKey A unique key for the job
+     * @param {function} obj.job The JS-function that will be run
+     *
+     * @example
+     * import BackgroundJob from 'react-native-background-job';
+     *
+     * const backgroundJob = {
+     *  jobKey: "myJob",
+     *  job: () => console.log("Running in background")
+     * };
+     *
+     * BackgroundJob.register(backgroundJob);
+     *
+     */
+  register: function({ jobKey, job }) {
+    const existingJob = registeredJobs[jobKey];
+
+    if (!existingJob || !existingJob.registered) {
+      var fn = async () => {
+        job();
+      };
+
+      AppRegistry.registerHeadlessTask(jobKey, () => fn);
+
+      if (existingJob) {
+        registeredJobs[jobKey].registered = true;
+      } else {
+        const scheduledJob = nativeJobs.filter(nJob => nJob.jobKey == jobKey);
+        const scheduled = scheduledJob[0] != undefined;
+        registeredJobs[jobKey] = { registered: true, scheduled };
+      }
+    }
+  },
+  /**
+     * Schedules a new job.
+     *
+     * This only has to be run once while `register` has to be run on each initialization of React Native.
+     *
+     * @param {Object} obj
+     * @param {string} obj.jobKey A unique key for the job
+     * @param {number} obj.timeout How long the JS job may run before being terminated by Android (in ms).
+     * @param {number} [obj.period = 900000] - The frequency to run the job with (in ms). This number is not exact, Android may modify it to save batteries. Note: For Android > N, the minimum is 900 0000 (15 min).
+     * @param {boolean} [obj.persist = true] If the job should persist over a device restart.
+     * @param {boolean} [obj.warn = true] If a warning should be raised if overwriting a job that was already scheduled.
+     * @param {number} [obj.networkType = BackgroundJob.NETWORK_TYPE_NONE] Only run for specific network requirements, (not respected by pre Android N devices) [docs](https://developer.android.com/reference/android/app/job/JobInfo.html#NETWORK_TYPE_ANY)
+     * @param {boolean} [obj.requiresCharging = false] Only run job when device is charging, (not respected by pre Android N devices) [docs](https://developer.android.com/reference/android/app/job/JobInfo.Builder.html#setRequiresCharging(boolean))
+     * @param {boolean} [obj.requiresDeviceIdle = false] Only run job when the device is idle, (not respected by pre Android N devices) [docs](https://developer.android.com/reference/android/app/job/JobInfo.Builder.html#setRequiresDeviceIdle(boolean))
+     *
+     * @example
+     * import BackgroundJob from 'react-native-background-job';
+     *
+     * const backgroundJob = {
+     *  jobKey: "myJob",
+     *  job: () => console.log("Running in background")
+     * };
+     *
+     * BackgroundJob.register(backgroundJob);
+     *
+     * var backgroundSchedule = {
+     *   taskId: 182,
+     *  jobKey: "myJob",
+     *  timeout: 5000
+     * }
+     *
+     * BackgroundJob.scheduleRepeatedJob(backgroundSchedule);
+     */
+  scheduleRepeatedJob: function(
+    { taskId,
+      jobKey,
+      timeout,
+      period = 900000,
+      persist = true,
+      warn = true,
+      networkType = this.NETWORK_TYPE_NONE,
+      requiresCharging = false,
+      requiresDeviceIdle = false,
+      payLoad = null
+    }
+  ) {
+    const registeredJob = registeredJobs[jobKey];
+
+    if (!registeredJob) {
+      console.error(
+        `${tag} The job ${jobKey} has not been registered, you must register it before you can schedule it.`
+      );
+    } else {
+      if(alreadyScheduled[taskId] && warn && globalWarning) {
+        console.warn(
+          `${tag} The job ${taskId} ${jobKey} is existing. Hence overwriting background job.`
+        );
+      }else{
+        alreadyScheduled[taskId] = jobKey;
+      }
+
+      AppState.getCurrentAppState(
+        ({ app_state }) => {
+          const appActive = app_state == "active";
+          jobModule.scheduleRepeatedJob(
+            taskId,
+            jobKey,
+            timeout,
+            period,
+            persist,
+            !appActive,
+            networkType,
+            requiresCharging,
+            requiresDeviceIdle,
+            payLoad
+          );
+        },
+        () => console.err(`${tag} Can't get Current App State`)
+      );
+    }
+  },
+
+  /*
+  Schedule OneTimeJob
+   */
+  scheduleOneTimeJob: function(
+    {
+      taskId,
+      jobKey,
+      timeout,
+      persist = true,
+      warn = true,
+      networkType = this.NETWORK_TYPE_NONE,
+      requiresCharging = false,
+      requiresDeviceIdle = false,
+      payLoad = null
+    }
+  ) {
+    const registeredJob = registeredJobs[jobKey];
+
+    if (!registeredJob) {
+      console.error(
+        `${tag} The job ${jobKey} has not been registered, you must register it before you can schedule it.`
+      );
+    } else {
+      if(alreadyScheduled[taskId] && warn && globalWarning) {
+        console.warn(
+          `${tag} The job ${taskId} ${jobKey} is existing. Hence overwriting background job.`
+        );
+      }else{
+        alreadyScheduled[taskId] = jobKey;
+      }
+
+      AppState.getCurrentAppState(
+        ({ app_state }) => {
+          const appActive = app_state == "active";
+          jobModule.scheduleOneTimeJob(
+            taskId,
+            jobKey,
+            timeout,
+            persist,
+            !appActive,
+            networkType,
+            requiresCharging,
+            requiresDeviceIdle,
+            payLoad
+          );
+        },
+        () => console.err(`${tag} Can't get Current App State`)
+      );
+    }
+  },
+
+
+  /**
+     * Fetches all the currently scheduled jobs
+     *
+     * @param {Object} obj
+     * @param {function(Array)} obj.callback A list of all the scheduled jobs will be passed to the callback
+     *
+     * @example
+     * import BackgroundJob from 'react-native-background-job';
+     *
+     * BackgroundJob.getAll({callback: (jobs) => console.log("Jobs:",jobs)});
+     *
+     */
+  getAll: function({ callback }) {
+    jobModule.getAll(callback);
+  },
+
+  /**
+     * Cancel a specific job
+     *
+     * @param {Object} obj
+     * @param {string} obj.jobKey The unique key for the job
+     * @param {int} obj.taskId The unique id for the task
+     * @param {boolean} [obj.warn = true] If one tries to cancel a job that has not been scheduled it will warn
+     *
+     * @example
+     * import BackgroundJob from 'react-native-background-job';
+     *
+     * BackgroundJob.cancel({jobKey: 'myJob', taskId: 182});
+     */
+  cancel: function({ jobKey, taskId, warn = true }) {
+
+    if (warn && globalWarning && !registeredJobs[jobKey]) {
+      console.warn(
+        `${tag} Trying to cancel the job ${jobKey} but it is not registered`
+      );
+    }
+    if (warn && globalWarning && !alreadyScheduled[taskId]) {
+      console.warn(
+        `${tag} Trying to cancel the job ${jobKey}{${taskId}} but it is not scheduled`
+      );
+    }
+    jobModule.cancel(jobKey, taskId);
+    delete alreadyScheduled[taskId];
+    // jobs[jobKey] ? jobs[jobKey].scheduled = false : null;
+
+  },
+
+   /**
+     * Hard cancel a specific job without worrying about jobkey
+     *
+     * @param {Object} obj
+     * @param {int} obj.taskId The unique id for the task
+     * @param {boolean} [obj.warn = true] If one tries to cancel a job that has not been scheduled it will warn
+     *
+     * @example
+     * import BackgroundJob from 'react-native-background-job';
+     *
+     * BackgroundJob.cancel({taskId: 182});
+     */
+  hardCancel: function({ taskId, warn = true }) {
+    if (warn && globalWarning && !registeredJobs[jobKey]) {
+      console.warn(
+        `${tag} Trying to cancel the job ${jobKey} but it is not registered`
+      );
+    }
+    jobModule.hardCancel(taskId);
+    delete alreadyScheduled[taskId];
+    // jobs[jobKey] ? jobs[jobKey].scheduled = false : null;
+  },
+
+
+  /**
+     * Cancels all the scheduled jobs
+     *
+     * @example
+     * import BackgroundJob from 'react-native-background-job';
+     *
+     * BackgroundJob.cancelAll();
+     */
+  cancelAll: function() {
+    jobModule.cancelAll();
+    const keys = Object.keys(alreadyScheduled);
+    keys.map(key => {
+      // jobs[key].scheduled = false;
+      delete alreadyScheduled[key];
+    });
+  },
+  /**
+     * Sets the global warning level
+     *
+     * @param {boolean} warn
+     *
+     * @example
+     * import BackgroundJob from 'react-native-background-job';
+     *
+     * BackgroundJob.setGlobalWarnings(false);
+     *
+     */
+  setGlobalWarnings: function(warn) {
+    globalWarning = warn;
+  }
+};
+module.exports = BackgroundJob;
